aaa7de1d54ca9e516185e386c2e8622ca672ac6a
Rafael Maneschy <rafamaneschy@gmail.com>
2021-05-04 18:03:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exercicios-logicaUri
+ Repositório criado para salvar resoluções de exercícios realizados no site Uri Online Judge. 
---
+++ exerciciosSiteUri/src/urionline1/Ex1001.java
@@ -0,0 +1,24 @@
+package urionline1;
+
+import java.util.Scanner;
+
+public class Ex1001 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, X;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		X = A + B;
+		
+		System.out.println("X = "+ X);		
+		
+		
+		sc.close();
+
+	}
+
+}
+
